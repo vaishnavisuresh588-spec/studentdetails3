1e8dd0490200bab8aac430b9c05a41de7322fb09
vaishnavisuresh588-spec <vaishnavisuresh588@gmail.com>
2025-11-04 11:45:44 +0530

    Update stu3.py

--- stu3.py
+++ stu3.py
@@ -3,12 +3,12 @@ if len(sys.argv) == 3:
   script name = sys.argv[0]
   name = sys.argv[1]
   rollno = sys.argv[2]
-  print("user provided input values:")
+   print("user provided input values:")
 else:
   script name = sys.argv[0]
   name = "vaishnavi"
   rollno = "14"
-  print("no input given - using default values:")
+   print("no input given - using default values:")
 print("Script name:", script name)
 print("Student name:", name)
 print("Roll no:", rollno)
